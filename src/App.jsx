005b5ec44d92c0f8919c7738ba84e165e0d5c14d
MatheusKyoshiShoji <matheus.shoji@sptech.school>
2024-09-07 16:59:54 -0300

    Projeto React + Vite inicializado
    - Dependências: Vite e Tailwind CSS
    - Componente App: Teste do tailwind

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <header className="w-full bg-green-600 p-8 flex justify-center">
+        <h1 className="text-white text-4xl"> Adoteme Website Started </h1>
+      </header>
+    </>
+  )
+}
+
+export default App
