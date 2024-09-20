fbc28c010592063a45e85fa90c8cfd97ddb5df6c
MatheusKyoshiShoji <matheus.shoji@sptech.school>
2024-09-20 20:39:19 -0300

    Mapa renderizando de acordo com a posição atual
    Observações:
    - Geolocalizador apresenta inconsistências
    - Precisa ser otimizar com o cache

--- src/components/Maps/index.jsx
+++ src/components/Maps/index.jsx
@@ -7,7 +7,7 @@ import {
   Pin,
   useAdvancedMarkerRef,
 } from "@vis.gl/react-google-maps";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { PlaceAutocomplete } from "../PlaceAutocomplete";
 import { MapHandler } from "../MapHandler";
 import ClusteredPetsMarkers from "../ClusterMarker";
@@ -17,66 +17,101 @@ import useGeolocation from "../../hooks/useGeolocation";
 import { useLocation } from "react-router-dom";
 
 const MapaAchados = () => {
+  const defaultPosition = { zoom: 19, center: { lat: -23.558052381604917, lng: -46.661807140459345 } };
+  
   const { position, error: geoError } = useGeolocation();
+  const { state } = useLocation();
 
-  const {state} = useLocation();
-  const defaultPosition = { lat: -23.43896506940708, lng: -46.53214115945167 };
-  const [mapCenter, setMapCenter] = useState(state?.mapCenter || defaultPosition);
-  const [pets, setPets] = useState([])
-
+  
+  const [cameraProps, setCameraProps] = useState(state?.mapCenter || defaultPosition);
+  const [pets, setPets] = useState([]);
   const [markerRef, marker] = useAdvancedMarkerRef();
-  const [selectedPlace, setSelectedPlace] = useState(null);;
+  const [selectedPlace, setSelectedPlace] = useState(null);
   const [nearbyPets, setNearbyPets] = useState([]);
   const [isShowingModal, toogleModal] = useModal();
 
-  const ClusteredPetsMarkersMemo = React.memo(ClusteredPetsMarkers);
-  const PlaceAutocompleteMemo = React.memo(PlaceAutocomplete);
   const ListAchadosMemo = React.memo(ListAchados);
 
-  useEffect(() => {
-    fetch("/pets.json")
-      .then((response) => response.json())
-      .then((data) => setPets(data))
-      .catch((error) => console.error("Erro json pets", error));
-  }, []);
+  const handleCameraChange = useCallback(
+    (ev) => setCameraProps(ev.detail),
+    []
+  );
 
 
   useEffect(() => {
-    if (position) {
-      setMapCenter(position);
+    if (position && !geoError) {
+      console.log(position)
+      setCameraProps((prevCamera) => ({
+        ...prevCamera,
+        center: { lat: position.lat, lng: position.lng },
+        zoom: 15,
+      }));
     } else if (geoError) {
-      setMapCenter(defaultPosition);
+      console.error("Geolocation error: ", geoError);
+      alert("Não foi possível acessar sua localização");
+      setCameraProps(defaultPosition); 
     }
   }, [position, geoError]);
 
+  
+  useEffect(() => {
+    const abortController = new AbortController();
+    fetch("/pets.json", { signal: abortController.signal })
+      .then((response) => response.json())
+      .then((data) => setPets(data))
+      .catch((error) => {
+        if (error.name === "AbortError") return;
+        console.error("Erro ao buscar pets.json", error);
+      });
+
+    return () => {
+      abortController.abort();
+    };
+  }, []);
+
   const handlePlaceSelect = (place) => {
     setSelectedPlace(place);
-    if (!isShowingModal) toogleModal(); 
+    if (!isShowingModal) toogleModal();
   };
 
   return (
     <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
       <div className="w-full flex justify-center">
-        <Map style={{ width: "100%", height: "100vh" }} zoom={19} mapId={import.meta.env.VITE_MAP_ID}
-          defaultCenter={mapCenter} disableDefaultUI={true} reuseMaps={true}>
+        <Map
+          style={{ width: "100%", height: "100vh" }}
+          zoom={cameraProps.zoom}
+          mapId={import.meta.env.VITE_MAP_ID}
+          center={cameraProps.center}
+          disableDefaultUI={true}
+          onCameraChanged={handleCameraChange}
+          reuseMaps={true}
+        >
           <MapControl position={ControlPosition.TOP}>
             <div className="m-6 bg-branco w-[500px] flex justify-center">
-              <PlaceAutocompleteMemo onPlaceSelect={handlePlaceSelect} />
+              <PlaceAutocomplete onPlaceSelect={handlePlaceSelect} />
             </div>
           </MapControl>
 
           <MapControl position={ControlPosition.RIGHT_CENTER}>
-            <ListAchadosMemo pets={nearbyPets} show={isShowingModal} onClose={toogleModal}/>
+            <ListAchadosMemo
+              pets={nearbyPets}
+              show={isShowingModal}
+              onClose={toogleModal}
+            />
           </MapControl>
 
-          <ClusteredPetsMarkersMemo pets={pets} />
+          <ClusteredPetsMarkers pets={pets} />
 
           <AdvancedMarker ref={markerRef} position={null} onClick={toogleModal}>
             <Pin />
           </AdvancedMarker>
-
         </Map>
-        <MapHandler place={selectedPlace} marker={marker} pets={pets} setNearbyPets={setNearbyPets}/>
+        <MapHandler
+          place={selectedPlace}
+          marker={marker}
+          pets={pets}
+          setNearbyPets={setNearbyPets}
+        />
       </div>
     </APIProvider>
   );
--- src/pages/Achados/index.jsx
+++ src/pages/Achados/index.jsx
@@ -1,14 +1,11 @@
-import { useNavigate } from "react-router-dom";
 import MapaAchados from "../../components/Maps";
 
 const Achados = () => {
-  const navigate = useNavigate();
-  const handleNavigate = () => {
-    navigate("/mapa-achados", { state: {mapCenter} });
-  }
-
-  return <MapaAchados/>
-  
+  return (
+    <>
+      <MapaAchados />
+    </>
+  );
 };
 
 export default Achados;
