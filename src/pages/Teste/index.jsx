dee23b07f20bb104110f1622159a137a431c9646
MatheusKyoshiShoji <142830615+MatheusKyoshiShoji@users.noreply.github.com>
2024-09-25 15:13:21 -0300

    Upload de imagem via RESTApi

--- src/pages/Teste/index.jsx
+++ src/pages/Teste/index.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
-import CloudImage from "../../components/common/CloudImage";
-import UploadWidget from "../../components/feature/UploadImage";
 import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAdd, faCamera } from "@fortawesome/free-solid-svg-icons";
 
 const EnviarImagem = () => {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [preview, setPreview] = useState(null);
   const [image, setImage] = useState("");
 
   const uwConfig = {
@@ -11,11 +13,16 @@ const EnviarImagem = () => {
     uploadPreset: import.meta.env.VITE_UPLOAD_PRESET,
   };
 
-  const uploadImage = (files) => {
-    const formData = new FormData();
+  const uploadImage = () => {
+    if (!selectedFile) {
+      console.error("No file selected.");
+      return;
+    }
 
-    formData.append("file", files[0]);
+    const formData = new FormData();
+    formData.append("file", selectedFile);
     formData.append("upload_preset", uwConfig.uploadPreset);
+
     axios
       .post(
         `https://api.cloudinary.com/v1_1/${uwConfig.cloudName}/image/upload`,
@@ -23,18 +30,57 @@ const EnviarImagem = () => {
       )
       .then((response) => {
         setImage(response.data.secure_url);
+        setPreview(null);
       })
       .catch((error) => {
         console.error("Error uploading the image:", error);
       });
   };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    uploadImage();
+  };
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    setSelectedFile(file);
+    if (file) {
+      setPreview(URL.createObjectURL(file));
+    }
+  };
+
   return (
-    <div>
-      {/* <UploadWidget uwConfig={uwConfig} setPublicId={setPublicId}/> */}
-      <input type="file" onChange={(e) => uploadImage(e.target.files)} />
-      <img src={image} alt="Imagem enviada" />
-    </div>
+    <form onSubmit={handleSubmit} className="flex flex-col items-center gap-10">
+      <div className="relative">
+        <div className="w-72 h-72 bg-cinza rounded-full flex items-center justify-center overflow-hidden">
+          {preview ? (
+            <img
+              src={preview}
+              alt="Preview"
+              className="object-cover w-full h-full"
+            />
+          ) : (
+            <FontAwesomeIcon icon={faAdd}/>
+          )}
+        </div>
+        <label
+          htmlFor="file-input"
+          className="bg-azul-dark text-branco rounded-full py-2 px-3 w-fit absolute top-2 right-6 cursor-pointer"
+        >
+          <FontAwesomeIcon icon={faCamera} />
+        </label>
+        <input
+          id="file-input"
+          type="file"
+          onChange={handleFileChange}
+          className="hidden"
+        />
+      </div>
+      <button type="submit" className="bg-azul-main px-4 py-2 w-fit">
+        Enviar
+      </button>
+    </form>
   );
 };
 
@@ -47,37 +93,3 @@ const Teste = () => {
 };
 
 export default Teste;
-
-/* import { useEffect, useState } from "react";
-import ListAchados from "../../components/ListAchados";
-import useModal from "../../hooks/useModal";
-import { getPetPerdido } from "../../services/pets";
-
-const Teste = () => {
-  const [pets, setPets] = useState([]);
-  const [isShowingModal, toogleModal] = useModal();
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await getPetPerdido();
-        console.log(response.data);
-        setPets(response.data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-    fetchData();
-  }, []);
-
-  return (
-    <>
-      <button onClick={toogleModal} className="bg-azul-main p-2">
-        Mostrar modal
-      </button>
-      <ListAchados pets={pets} show={isShowingModal} onClose={toogleModal} />
-    </>
-  );
-};
-
-export default Teste; */
