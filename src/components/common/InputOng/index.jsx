52a74dd9134944d1712c5ba9d720da997cee5d38
Matheus Shoji <142830615+MatheusKyoshiShoji@users.noreply.github.com>
2024-10-28 11:55:11 -0300

    Campo de Pesquisa tabela ONG

--- src/components/common/InputOng/index.jsx
+++ src/components/common/InputOng/index.jsx
@@ -1,20 +1,37 @@
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
 
-const InputOng = () => {
+const InputOng = ({ setTableData, originalData }) => {
+  const [searchText, setSearchText] = useState("");
+
+  const requestSearch = (searchValue) => {
+    setSearchText(searchValue);
+
+    const searchRegex = new RegExp(`.*${searchValue}.*`, "ig");
+    const filteredRows = originalData.filter((o) => {
+      return Object.keys(o).some((k) => {
+        return searchRegex.test(o[k].toString());
+      });
+    });
+    setTableData(filteredRows);
+  };
+
   return (
     <div className="relative block">
       <label htmlFor="Search" className="sr-only">
         Pesquisa
       </label>
       <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-        <span className="sr-only"> Pesquisa </span>
+        <span className="sr-only">Pesquisa</span>
         <FontAwesomeIcon icon={faSearch} />
       </span>
       <input
         type="text"
         id="pesquisa"
         placeholder="Pesquisar..."
+        value={searchText}
+        onChange={(e) => requestSearch(e.target.value)}
         className="w-full rounded-full bg-input-d py-2.5 pe-8 pl-9 focus:outline-none block"
       />
     </div>
--- src/pages/OngPet/index.jsx
+++ src/pages/OngPet/index.jsx
@@ -10,60 +10,65 @@ import { Link } from "react-router-dom";
 
 const OngPet = () => {
   const [dataPets, setDataPets] = useState([]);
+  const [filteredPets, setFilteredPets] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('/petsOng.json');
+        const response = await axios.get("/petsOng.json");
         const data = response.data;
 
         const dataFormat = data.map((pet) => ({
           ...pet,
-          visibilidade: pet.visibilidade ? 'Visível' : 'Escondido',
+          visibilidade: pet.visibilidade ? "Visível" : "Escondido",
         }));
 
         setDataPets(dataFormat);
+        setFilteredPets(dataFormat);
       } catch (error) {
-        if (error.name === 'AbortError') return;
-        console.error('Erro ao buscar os dados da API', error);
+        if (error.name === "AbortError") return;
+        console.error("Erro ao buscar os dados da API", error);
       }
     };
 
     fetchData();
-
   }, []);
 
   const exportarPets = async (id) => {
     try {
       const response = await exportacaoPets(id);
-
       const href = URL.createObjectURL(response.data);
 
-      const link = document.createElement('a');
+      const link = document.createElement("a");
       link.href = href;
-      link.setAttribute('download', 'pets.csv');
+      link.setAttribute("download", "pets.csv");
       document.body.appendChild(link);
       link.click();
 
       document.body.removeChild(link);
       URL.revokeObjectURL(href);
-
     } catch (err) {
-      console.log("Deu ruim, " + err);
+      console.log("Não foi possível realizar o download, " + err);
     }
-
-  }
+  };
 
   return (
     <>
       <PageTitle title="Pets" actionName="+ Adicionar pet">
-        <button className="font-nunito px-3 py-2 rounded-lg bg-azul-main text-branco" onClick={() => exportarPets(1)}>Exportar registros</button>
-        <Link className="font-nunito px-3 py-2 rounded-lg bg-amarelo-select text-branco">+ Adicionar pet</Link>
+        <button
+          className="font-nunito px-3 py-2 rounded-lg bg-azul-main text-branco"
+          onClick={() => exportarPets(1)}
+        >
+          Exportar registros
+        </button>
+        <Link className="font-nunito px-3 py-2 rounded-lg bg-amarelo-select text-branco">
+          + Adicionar pet
+        </Link>
       </PageTitle>
-      <SearchLayout numberResults={dataPets.length} registerName="Pets">
-        <InputOng />
+      <SearchLayout numberResults={filteredPets.length} registerName="Pets">
+        <InputOng setTableData={setFilteredPets} originalData={dataPets} />
       </SearchLayout>
-      <TableOng rows={dataPets} columns={petsColumns} />
+      <TableOng rows={filteredPets} columns={petsColumns} />
     </>
   );
 };
