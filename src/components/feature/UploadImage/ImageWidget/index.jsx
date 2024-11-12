eba212cb44464010c250f5987724e1bbdec29b59
Matheus Shoji <142830615+MatheusKyoshiShoji@users.noreply.github.com>
2024-11-12 16:40:21 -0300

    Loading para preenchimento de imagens

--- src/components/feature/UploadImage/ImageWidget/index.jsx
+++ src/components/feature/UploadImage/ImageWidget/index.jsx
@@ -2,15 +2,28 @@ import { faCamera } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState, useEffect } from "react";
 import { Controller } from "react-hook-form";
+import { MoonLoader } from "react-spinners";
 
 const ImageWidget = ({ control, onChange, image, colSpan, contextPath }) => {
   const [preview, setPreview] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     if (image) {
-      setPreview(URL.createObjectURL(image));
+      setLoading(true);
+      const objectURL = URL.createObjectURL(image);
+      setPreview(objectURL);
+      return () => URL.revokeObjectURL(objectURL);
     }
   }, [image]);
+
+  useEffect(() => {
+    if (preview) {
+      const img = new Image();
+      img.src = preview;
+      img.onload = () => setLoading(false);
+    }
+  }, [preview])
 
   const handleFileChange = (file, fieldOnChange) => {
     if (file && file.size > 5 * 1024 * 1024) {
@@ -33,10 +46,14 @@ const ImageWidget = ({ control, onChange, image, colSpan, contextPath }) => {
   return (
     <div className={`flex justify-center items-center shadow-xl bg-cinza 
     rounded-md relative ${contextPath === 'abrigo' ? colSpan : 'w-1/2'}`}>
-      {preview ? (
+      {loading ? (
+        <div className="absolute inset-0 flex justify-center items-center bg-black bg-opacity-50 rounded-md text-white">
+          <MoonLoader speedMultiplier={1}/>
+        </div>
+      ) : preview ? (
         <img src={preview} alt="Preview" className="object-fill h-full w-full rounded-md" />
       ) : (
-        <FontAwesomeIcon icon={faCamera} size="2x"/>
+        <FontAwesomeIcon icon={faCamera} size="2x" />
       )}
       <Controller
         name="imagem"
