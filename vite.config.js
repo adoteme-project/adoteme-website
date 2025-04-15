ec018d444ca48f2737c76e22a8fc7f42c28d21c7
Matheus Shoji <142830615+MatheusKyoshiShoji@users.noreply.github.com>
2025-04-15 19:13:39 -0300

    fix base url

--- vite.config.js
+++ vite.config.js
@@ -2,22 +2,24 @@ import { defineConfig } from "vite";
 import * as path from "path";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [
-      { find: "@", replacement: path.resolve(__dirname, "src") },
-      { find: "@maps", replacement: path.resolve(__dirname, "src/components/feature/Maps") },
-    ],
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: "adotme-api-hjg3egfuhhgzecdw.brazilsouth-01.azurewebsites.net",
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === "production" ? "http://prod" : "http://localhost:8080",
+    plugins: [react()],
+    resolve: {
+      alias: [
+        { find: "@", replacement: path.resolve(__dirname, "src") },
+        { find: "@maps", replacement: path.resolve(__dirname, "src/components/feature/Maps") },
+      ],
+    },
+    server: {
+      proxy: {
+        "/api": {
+          target: process.env.VITE_API_URL || "http://localhost:8080",
+          changeOrigin: true,
+          secure: false
+        },
       },
     },
-  },
+  };
 });
